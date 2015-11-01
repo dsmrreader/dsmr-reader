45652dec0a55855a65546b52f76e0d8ff815f602
Dennis Siemensma <dennissiemensma@users.noreply.github.com>
2015-11-02 00:13:38 +0100

    Prod config (see header)

---
+++ dsmrreader/config/production.py
@@ -0,0 +1,10 @@
+"""
+Production configs should not be committed into version control.
+However, since this project is localhost only, I do not care ;-)
+"""
+
+from dsmrreader.config.development import *
+
+DEBUG = False
+
+# DATABASES = Copied from development.
