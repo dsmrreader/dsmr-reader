bec84284583247d15cefb8577e71b89c949b7e33
Dennis Siemensma <github@dennissiemensma.nl>
2015-11-15 23:42:50 +0100

    Added dashboard & recent usage screen.

--- dsmrreader/urls.py
+++ dsmrreader/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^stats/', include('dsmr_stats.urls', namespace='stats'))
+    url(r'^', include('dsmr_stats.urls', namespace='stats'))
 ]
