e0b22ea56fecb32fc0146b8106baad28e0ed5463
dennis <dennis@localhost>
2015-11-18 16:27:53 +0000

    Added deploy script.

--- reload.sh
+++ reload.sh
@@ -2,3 +2,4 @@
 
 # Sending a HANGUP signal to Gunicorn's master process will gracefully reload its children.
 cat /var/tmp/gunicorn--dsmr_webinterface.pid | xargs kill -HUP
+echo "Gunicorn reloaded."
