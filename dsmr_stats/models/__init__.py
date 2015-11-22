17f817ed9d021d9fe7313e56ba00125a47618f0a
Dennis Siemensma <dennissiemensma@users.noreply.github.com>
2015-11-22 16:09:47 +0100

    Splitted models class among multiple files.

---
+++ dsmr_stats/models/__init__.py
@@ -0,0 +1,4 @@
+from .consumption import ElectricityConsumption, GasConsumption
+from .dsmrreading import DsmrReading
+from .energysupplier import EnergySupplierPrice
+from .statistics import ElectricityStatistics
