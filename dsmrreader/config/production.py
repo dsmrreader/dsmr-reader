65976d2c2b81931c0f5d61a396aefa6dda3a78f9
dennis <dennis@localhost>
2015-12-11 22:17:02 +0100

    Splitted production config templates as well.

--- dsmrreader/config/production.py
+++ dsmrreader/config/production.py
@@ -8,18 +8,7 @@ from dsmrreader.config.development import *
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-### Choose either MySQL/MariaDB or PostgreSQL. 
-### SQLite should work as well, but is untested!
-#        'ENGINE': 'django.db.backends.mysql',
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dsmrreader',
-        'USER': 'dsmrreader',
-        'PASSWORD': 'dsmrreader',
-        'CONN_MAX_AGE': 300,
-    }
-}
+DATABASES = None  # Please use one of the prepared configs per database backend.
 
 STATIC_ROOT = '/var/www/dsmrreader/static'
 
