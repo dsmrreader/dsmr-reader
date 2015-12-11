e16836658b2be63b72ca97ec6353f2a1d63707eb
dennis <dennis@localhost>
2015-12-11 21:57:17 +0100

    Production config now suggests database backend.

--- dsmrreader/config/production.py
+++ dsmrreader/config/production.py
@@ -8,7 +8,18 @@ from dsmrreader.config.development import *
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-# DATABASES = Copied from development.
+DATABASES = {
+    'default': {
+### Choose either MySQL/MariaDB or PostgreSQL. 
+### SQLite should work as well, but is untested!
+#        'ENGINE': 'django.db.backends.mysql',
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dsmrreader',
+        'USER': 'dsmrreader',
+        'PASSWORD': 'dsmrreader',
+        'CONN_MAX_AGE': 300,
+    }
+}
 
 STATIC_ROOT = '/var/www/dsmrreader/static'
 
