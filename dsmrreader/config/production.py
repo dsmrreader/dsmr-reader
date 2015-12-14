e1eacd3312783c61ca4abec021494c9121138228
Dennis Siemensma <dennissiemensma@users.noreply.github.com>
2015-12-14 22:11:06 +0100

    Added warning regarding SECRET_KEY for public production usage.

---
+++ dsmrreader/config/production.py
@@ -0,0 +1,15 @@
+"""
+Production configs should not be committed into version control.
+However, since this project is localhost only, I do not care ;-)
+"""
+
+from dsmrreader.config.development import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+DATABASES = None  # Please use one of the prepared configs per database backend.
+
+STATIC_ROOT = '/var/www/dsmrreader/static'
+
+CACHES['default']['TIMEOUT'] = 5 * 60
