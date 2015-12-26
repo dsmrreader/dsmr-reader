97ebdb27092cf49c00234b7f79ec4b4c9e38f87a
Dennis Siemensma <github@dennissiemensma.nl>
2015-12-26 15:37:25 +0100

    Moved regression test to own location.

--- dsmr_stats/tests/test_webinterface.py
+++ dsmr_stats/tests/test_webinterface.py
@@ -72,18 +72,3 @@ class TestViews(TestCase):
             reverse('{}:energy-supplier-prices'.format(self.namespace))
         )
         self.assertEqual(response.status_code, 200)
-
-
-class TestRegression(TestCase):
-    """ Regression. """
-    fixtures = ['test_dsmrreading.json']
-
-    def setUp(self):
-        self.client = Client()
-
-    def test_energysupplierprice_matching_query_does_not_exist(self):
-        call_command('dsmr_stats_compactor')
-        # This used to raise EnergySupplierPrice.DoesNotExist when no price fixtures are loaded.
-        self.client.get(
-            reverse('stats:dashboard')
-        )
