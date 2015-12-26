22bc4763e2971981d77a7af09d5cacb5891b31d7
Dennis Siemensma <dennissiemensma@users.noreply.github.com>
2015-12-26 19:12:57 +0100

    #11 | Added model for keeping track of notes.

--- dsmr_stats/admin.py
+++ dsmr_stats/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
 from dsmr_stats.models import EnergySupplierPrice
+from dsmr_stats.models.note import Note
 
 
-admin.site.register(EnergySupplierPrice)
+@admin.register(EnergySupplierPrice)
+class EnergySupplierPriceAdmin(admin.ModelAdmin):
+    list_display = ('description', 'start', 'end')
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('description', 'day')
