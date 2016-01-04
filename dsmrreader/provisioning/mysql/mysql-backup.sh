f18f59e9184d95fbf3ce6da500ebdaa1f13bb2b3
dennis <dennis@localhost>
2016-01-04 22:32:47 +0100

    Improved backup scripts' instructions.

--- dsmrreader/provisioning/mysql/mysql-backup.sh
+++ dsmrreader/provisioning/mysql/mysql-backup.sh
@@ -5,12 +5,13 @@
 ###
 ### COPY this file and schedule it daily by root user, do not symlink!
 ###
-### Howto: Place a file in /etc/cron.daily/database-backup, only containing these two lines:
+### Howto: Create a file in /etc/cron.daily/database-backup only containing these two lines:
 ### (asumes you copied the script to '/root/mysql-backup.sh' in this example)
 ###
 ### 	#!/bin/sh
 ### 	/root/mysql-backup.sh
 ###
+### Make sure it's owned by root and is executable: chmod 755 /etc/cron.daily/database-backup
 ### VERIFY your cronjob and its result once by executing: run-parts -v /etc/cron.daily
 ##################################################################################################
 
