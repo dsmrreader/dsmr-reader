0ded2b65055df30896b51c0895b00b15e10e1b46
dennis <dennis@localhost>
2016-01-04 20:34:32 +0100

    Readme update.

--- reload.sh
+++ reload.sh
@@ -2,4 +2,4 @@
 
 # Sending a HANGUP signal to Gunicorn's master process will gracefully reload its children.
 cat /var/tmp/gunicorn--dsmr_webinterface.pid | xargs kill -HUP
-echo "Gunicorn reloaded."
+echo "Reloaded Gunicorn process running the webinterface. Other processes will be reloaded automatically on next run."
