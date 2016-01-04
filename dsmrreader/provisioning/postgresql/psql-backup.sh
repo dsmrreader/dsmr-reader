57008b4cd0f571ac1768ca737d568fbce969f917
Dennis Siemensma <github@dennissiemensma.nl>
2016-01-04 22:32:47 +0100

    Improved backup scripts' instructions.

--- dsmrreader/provisioning/postgresql/psql-backup.sh
+++ dsmrreader/provisioning/postgresql/psql-backup.sh
@@ -5,13 +5,15 @@
 ###
 ### COPY this file and schedule it daily by root user, do not symlink!
 ###
-### Howto: Place a file in /etc/cron.daily/database-backup, only containing these two lines:
+### Howto: Create a file in /etc/cron.daily/database-backup only containing these two lines:
 ### (asumes you copied the script to '/root/psql-backup.sh' in this example)
 ###
 ### 	#!/bin/sh
 ### 	/root/psql-backup.sh
 ###
+### Make sure it's owned by root and is executable: chmod 755 /etc/cron.daily/database-backup
 ### VERIFY your cronjob and its result once by executing: run-parts -v /etc/cron.daily
+### You can easily check the backup file's content with either 'zcat' or 'view' commands.
 ##################################################################################################
 
 DIRECTORY="/data"
