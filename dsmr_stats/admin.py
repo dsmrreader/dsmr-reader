c137f5d18928bbc7774855ed9b64303e64e41613
Dennis Siemensma <github@dennissiemensma.nl>
2016-01-06 22:44:56 +0100

    Merge with 2c24a12554d29de40e8ffc09aec301376987e47a

--- dsmr_stats/admin.py
+++ dsmr_stats/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from solo.admin import SingletonModelAdmin
+
 
 from dsmr_stats.models.energysupplier import EnergySupplierPrice
 from dsmr_stats.models.note import Note
+from dsmr_stats.models.settings import StatsSettings
 
 
 @admin.register(EnergySupplierPrice)
@@ -12,3 +15,8 @@ class EnergySupplierPriceAdmin(admin.ModelAdmin):
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('day', 'description')
+
+
+@admin.register(StatsSettings)
+class StatsSettingsAdmin(SingletonModelAdmin):
+    list_display = ('compactor_grouping_type', )
