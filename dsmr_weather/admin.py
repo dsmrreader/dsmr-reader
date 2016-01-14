d2959771a66fb0415468ecca9cbed68b78cd7fbd
Dennis Siemensma <github@dennissiemensma.nl>
2016-01-14 19:33:25 +0100

    #18 | Initial setup for dsmr_weather.

---
+++ dsmr_weather/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from solo.admin import SingletonModelAdmin
+
+
+from dsmr_weather.models.statistics import TemperatureReading
+from dsmr_weather.models.settings import WeatherSettings
+
+
+@admin.register(TemperatureReading)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('read_at', 'temperature_celcius')
+
+
+@admin.register(WeatherSettings)
+class WeatherSettingsAdmin(SingletonModelAdmin):
+    list_display = ('track', 'buienradar_station')
