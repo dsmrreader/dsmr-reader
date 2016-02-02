c6198b04c6426c15195418e26b52e47f1434c23d
Dennis Siemensma <github@dennissiemensma.nl>
2016-02-03 00:52:11 +0100

    #19 | Initial (working) setup of data migrations.

--- dsmr_frontend/tests/models/test_settings.py
+++ dsmr_frontend/tests/models/test_settings.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 
-from dsmr_stats.models.settings import StatsSettings
+from dsmr_frontend.models.settings import FrontendSettings
 
 
 class TestSettings(TestCase):
     """ Tests for settings defaults. """
     def setUp(self):
-        self.instance = StatsSettings().get_solo()
+        self.instance = FrontendSettings().get_solo()
 
     def test_reverse_dashboard_graphs(self):
         self.assertFalse(self.instance.reverse_dashboard_graphs)
