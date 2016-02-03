161400986632c7f073d618595ed6aa29a13ffb32
Dennis Siemensma <github@dennissiemensma.nl>
2016-02-03 20:36:00 +0100

    #19 | Fixed model meta translations for admin, as they needed to be lazy.

--- dsmr_consumption/models/energysupplier.py
+++ dsmr_consumption/models/energysupplier.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models import Q
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class EnergySupplierPriceManager(models.Manager):
