0d07f89d834b9309782ef880e7960db0917caf94
Dennis Siemensma <dennissiemensma@users.noreply.github.com>
2016-02-06 00:16:02 +0100

    #3 | Dropped view mixins, separated views over multiple modules.

--- dsmr_frontend/urls.py
+++ dsmr_frontend/urls.py
@@ -2,21 +2,16 @@ from django.views.decorators.cache import cache_page
 from django.conf import settings
 from django.conf.urls import url
 
-from . import views
-
+from dsmr_frontend.views.dashboard import Dashboard
+from dsmr_frontend.views.history import History
+from dsmr_frontend.views.statistics import Statistics
 
 urlpatterns = [
-    url(r'^$', views.Dashboard.as_view(), name='dashboard'),
+    url(r'^$', Dashboard.as_view(), name='dashboard'),
     url(
         r'^history$',
-        cache_page(settings.CACHES['default']['TIMEOUT'])(
-            views.History.as_view()
-        ),
+        cache_page(settings.CACHES['default']['TIMEOUT'])(History.as_view()),
         name='history'
     ),
-    url(
-        r'^statistics$',
-        views.Statistics.as_view(),
-        name='statistics'
-    ),
+    url(r'^statistics$', Statistics.as_view(), name='statistics'),
 ]
