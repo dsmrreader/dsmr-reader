32fcc1e04f220907673a907d268dfc1761cf6dfe
Dennis Siemensma <github@dennissiemensma.nl>
2016-02-14 16:20:17 +0100

    #46 | Improved migrations to use latest reading as start.

--- dsmr_datalogger/migrations/0002_split_dsmr_reading_fields.py
+++ dsmr_datalogger/migrations/0002_split_dsmr_reading_fields.py
@@ -7,9 +7,28 @@ from django.db import migrations, models
 def initial_meter_statistics(apps, schema_editor):
     """ Guarantees there is at least one instance in the database, which will be updated later. """
     MeterStatistics = apps.get_model('dsmr_datalogger', 'MeterStatistics')
+    DsmrReading = apps.get_model('dsmr_datalogger', 'DsmrReading')
+
     # We can't (and shouldn't) use Solo here.
-    MeterStatistics.objects.create()  # All fields are NULL in database, by design.
+    stats = MeterStatistics.objects.create()  # All fields are NULL in database, by design.
     assert MeterStatistics.objects.exists()
+
+    try:
+        # Just use the latest DSMR reading, if any.
+        latest_reading = DsmrReading.objects.all().order_by('-timestamp')[0]
+    except IndexError:
+        return
+
+    stats.electricity_tariff = latest_reading.electricity_tariff
+    stats.power_failure_count = latest_reading.power_failure_count
+    stats.long_power_failure_count = latest_reading.long_power_failure_count
+    stats.voltage_sag_count_l1 = latest_reading.voltage_sag_count_l1
+    stats.voltage_sag_count_l2 = latest_reading.voltage_sag_count_l2
+    stats.voltage_sag_count_l3 = latest_reading.voltage_sag_count_l3
+    stats.voltage_swell_count_l1 = latest_reading.voltage_swell_count_l1
+    stats.voltage_swell_count_l2 = latest_reading.voltage_swell_count_l2
+    stats.voltage_swell_count_l3 = latest_reading.voltage_swell_count_l3
+    stats.save()
 
 
 class Migration(migrations.Migration):
@@ -41,6 +60,7 @@ class Migration(migrations.Migration):
                 'default_permissions': (),
             },
         ),
+        migrations.RunPython(initial_meter_statistics),
         migrations.AlterModelOptions(
             name='dsmrreading',
             options={'verbose_name': 'DSMR reading', 'default_permissions': (), 'ordering': ['timestamp']},
@@ -91,5 +111,4 @@ class Migration(migrations.Migration):
             name='track_meter_statistics',
             field=models.BooleanField(default=True, verbose_name='Track meter statistics', help_text='Whether we should track any extra statistics sent by the meter, such as the number of power failures of voltage dips. Data is not required for core features.'),
         ),
-        migrations.RunPython(initial_meter_statistics),
     ]
