67d012a5a3324f9056132e1623766b42b501a5d0
Dennis Siemensma <github@dennissiemensma.nl>
2016-02-14 16:21:18 +0100

    #46 | Improved GUI to check and hint for optional features when disabled

--- dsmr_frontend/views/statistics.py
+++ dsmr_frontend/views/statistics.py
@@ -2,8 +2,9 @@ from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.utils import timezone
 
-from dsmr_datalogger.models.reading import DsmrReading
+from dsmr_datalogger.models.reading import DsmrReading, MeterStatistics
 from dsmr_consumption.models.energysupplier import EnergySupplierPrice
+from dsmr_datalogger.models.settings import DataloggerSettings
 
 
 class Statistics(TemplateView):
@@ -15,6 +16,8 @@ class Statistics(TemplateView):
         context_data['latest_reading'] = DsmrReading.objects.all().order_by('-pk')[0]
         context_data['first_reading'] = DsmrReading.objects.all().order_by('pk')[0]
         context_data['total_reading_count'] = DsmrReading.objects.count()
+        context_data['datalogger_settings'] = DataloggerSettings.get_solo()
+        context_data['meter_statistics'] = MeterStatistics.get_solo()
 
         try:
             context_data['energy_prices'] = EnergySupplierPrice.objects.by_date(today)
