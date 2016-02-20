15c503adc2b80b5f12b5a238163f38190a67d180
Dennis Siemensma <github@dennissiemensma.nl>
2016-02-20 19:06:13 +0100

    Implement automatic backups & Dropbox cloud storage #44

--- dsmr_backup/migrations/0001_initial.py
+++ dsmr_backup/migrations/0001_initial.py
@@ -14,27 +14,27 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='BackupSettings',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
-                ('daily_backup', models.BooleanField(verbose_name='Backup daily', help_text='Create a backup of your data daily. Stored locally, but can be exported using Dropbox.', default=True)),
-                ('compress', models.BooleanField(verbose_name='Compress', help_text='Create backups in compressed (gzip) format, saving a significant amount of disk space.', default=True)),
-                ('backup_time', models.TimeField(verbose_name='Backup timestamp', help_text='Daily moment of creating the backup. You should prefer a nightly timestamp, as it might freeze or lock the application shortly during backup creation.', default=datetime.time(2, 0))),
-                ('latest_backup', models.DateTimeField(null=True, help_text='Timestamp of latest backup created. Automatically updated by application. Please note that the application will ignore the "backup_time" setting the first time used.', default=None, blank=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('daily_backup', models.BooleanField(verbose_name='Backup daily', default=True, help_text='Create a backup of your data daily. Stored locally, but can be exported using Dropbox.')),
+                ('compress', models.BooleanField(verbose_name='Compress', default=True, help_text='Create backups in compressed (gzip) format, saving a significant amount of disk space.')),
+                ('backup_time', models.TimeField(verbose_name='Backup timestamp', default=datetime.time(2, 0), help_text='Daily moment of creating the backup. You should prefer a nightly timestamp, as it might freeze or lock the application shortly during backup creation.')),
+                ('latest_backup', models.DateTimeField(null=True, verbose_name='Latest backup', blank=True, default=None, help_text='Timestamp of latest backup created. Automatically updated by application. Please note that the application will ignore the "backup_time" setting the first time used.')),
             ],
             options={
-                'verbose_name': 'Backup configuration',
                 'default_permissions': (),
+                'verbose_name': 'Backup configuration',
             },
         ),
         migrations.CreateModel(
             name='DropboxSettings',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
-                ('access_token', models.CharField(verbose_name='Dropbox access token', null=True, max_length=128, help_text='The access token for your Dropbox account. You should register an App for your own Dropbox account (https://www.dropbox.com/developers/apps). Please select "Permission type" named "App folder" to restrict unneeded access. Backups will be synced to a dedicated folder in your account. After creating your App you should be able to generate an "Access token" and enter it here. See https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/ for more information.', default=None, blank=True)),
-                ('latest_sync', models.DateTimeField(null=True, help_text='Timestamp of latest sync with Dropbox. Automatically updated by application.', default=None, blank=True)),
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('access_token', models.CharField(null=True, verbose_name='Dropbox access token', help_text='The access token for your Dropbox account. You should register an App for your own Dropbox account (https://www.dropbox.com/developers/apps). Please select "Permission type" named "App folder" to restrict unneeded access. Backups will be synced to a dedicated folder in your account. After creating your App you should be able to generate an "Access token" and enter it here. See https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/ for more information.', blank=True, max_length=128, default=None)),
+                ('latest_sync', models.DateTimeField(null=True, verbose_name='Latest sync', blank=True, default=None, help_text='Timestamp of latest sync with Dropbox. Automatically updated by application.')),
             ],
             options={
-                'verbose_name': 'Dropbox configuration',
                 'default_permissions': (),
+                'verbose_name': 'Dropbox configuration',
             },
         ),
     ]
---
+++ dsmr_backup/tests/models/test_settings.py
@@ -0,0 +1,44 @@
+from datetime import time
+
+from django.test import TestCase
+from django.contrib.admin.sites import site
+
+from dsmr_backup.models.settings import BackupSettings, DropboxSettings
+
+
+class TestBackupSettings(TestCase):
+    """ Tests for settings defaults. """
+    def setUp(self):
+        self.instance = BackupSettings().get_solo()
+
+    def test_admin(self):
+        """ Model should be registered in Django Admin. """
+        self.assertTrue(site.is_registered(BackupSettings))
+
+    def test_daily_backup(self):
+        self.assertTrue(self.instance.daily_backup)
+
+    def test_compress(self):
+        self.assertTrue(self.instance.compress)
+
+    def test_backup_time(self):
+        self.assertTrue(self.instance.backup_time, time(hour=2))
+
+    def test_latest_backup(self):
+        self.assertIsNone(self.instance.latest_backup)
+
+
+class TestDropboxSettings(TestCase):
+    """ Tests for settings defaults. """
+    def setUp(self):
+        self.instance = DropboxSettings().get_solo()
+
+    def test_admin(self):
+        """ Model should be registered in Django Admin. """
+        self.assertTrue(site.is_registered(DropboxSettings))
+
+    def test_access_token(self):
+        self.assertIsNone(self.instance.access_token)
+
+    def test_latest_sync(self):
+        self.assertIsNone(self.instance.latest_sync)
