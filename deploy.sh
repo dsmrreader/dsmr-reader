825b9516c00edd744b4e0f4ab918d4e2b8bd6b03
Dennis Siemensma <github@dennissiemensma.nl>
2016-02-22 21:22:50 +0100

    Changed deploy script order (migrations before statics)

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ echo " --- Checking & synchronizing base requirements for changes."
 pip3 install -r dsmrreader/provisioning/requirements/base.txt
 
 echo ""
-echo " --- Checking & synchronizing static file changes."
-./manage.py collectstatic --noinput
-
-echo ""
 echo " --- Checking & synchronizing database changes/migrations."
 ./manage.py migrate
+
+echo ""
+echo " --- Checking & synchronizing static file changes."
+./manage.py collectstatic --noinput
 
 echo ""
 echo " --- Sending an SIGHUP to Gunicorn process to gracefully reload itself (webinterface)."
