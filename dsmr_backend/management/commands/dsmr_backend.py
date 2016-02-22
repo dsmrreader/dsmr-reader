63bb4b859609c73ccedcdf8290822d267c09c9a4
Dennis Siemensma <github@dennissiemensma.nl>
2016-02-22 21:32:17 +0100

    Improved output of dsmr_backend to reflect receiver called.

--- dsmr_backend/management/commands/dsmr_backend.py
+++ dsmr_backend/management/commands/dsmr_backend.py
@@ -8,14 +8,19 @@ class Command(BaseCommand):
     help = _('Generates a generic event triggering apps for backend operations, cron-like.')
 
     def handle(self, **options):
-        self.stdout.write('Broadcasting "backend_called" signal...')
+        self.stdout.write('Broadcasting backend signal...')
 
         # send_robust() guarantees the every listener receives this signal.
         responses = dsmr_backend.signals.backend_called.send_robust(None)
 
-        for current_app, current_response in responses:
-            # Reflect any error to output for convenience.
-            if isinstance(current_response, Exception):
-                raise CommandError('Exception @ {}: {}'.format(current_app, current_response))
+        signal_failure = []
 
-        self.stdout.write('Finished.')
+        for current_receiver, current_response in responses:
+            print(' - {} :: {}'.format(current_receiver, current_response))
+
+            if isinstance(current_response, Exception):
+                signal_failure.append(current_response)
+
+        if signal_failure:
+            # Reflect any error to output for convenience.
+            raise CommandError(signal_failure)
