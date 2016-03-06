55c084f82f4e883b55bd4a73aaad5d06a58b1303
Dennis Siemensma <github@dennissiemensma.nl>
2016-03-06 21:46:59 +0100

    Show exceptions in production (webinterface) #87

---
+++ dsmr_frontend/middleware/exception_traceback.py
@@ -0,0 +1,17 @@
+import traceback
+
+from django.http.response import HttpResponseServerError
+from django.template.loader import render_to_string
+
+
+class ExceptionTracebackMiddleware(object):
+    """ Handles any uncaught exceptions crashing the webinterface, and displays them. """
+    def process_exception(self, request, exception):
+        return HttpResponseServerError(
+            render_to_string(
+                '500.html', {
+                    'exception': exception,
+                    'error_trace': traceback.format_tb(exception.__traceback__, limit=100)
+                }
+            )
+        )
