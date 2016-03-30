33c5a77b47d33b39ab0c65e1858aa491a2303853
Dennis Siemensma <github@dennissiemensma.nl>
2016-03-30 20:01:57 +0200

    Move documentation to wiki or RTD #90

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:48,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to DSMR Reader&#8217;s documentation!"],titleterms:{document:0,dsmr:0,indic:0,reader:0,tabl:0,welcom:0}})
