0458452705a9285a5e0f6bb82b03c0ae3d7cdede
Dennis Siemensma <github@dennissiemensma.nl>
2016-03-30 20:05:09 +0200

    Move documentation to wiki or RTD #90

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,6 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
