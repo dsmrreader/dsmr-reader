49a4ff8aee2a352a47288d22ec6727e0079bee0c
Dennis Siemensma <github@dennissiemensma.nl>
2018-01-17 19:18:42 +0100

    One-click installer #203

---
+++ dsmrreader/provisioning/installer/install_dsmrreader_raspbian.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -x
+
+# Packages
+sudo apt-get install -y postgresql postgresql-server-dev-all nginx supervisor git python3 python3-pip python3-virtualenv virtualenvwrapper
+
+# Database
+sudo sudo -u postgres createuser -DSR dsmrreader
+sudo sudo -u postgres createdb -O dsmrreader dsmrreader
+sudo sudo -u postgres psql -c "alter user dsmrreader with password 'dsmrreader';"
+
+# System user
+sudo useradd dsmr --home-dir /home/dsmr --create-home --shell /bin/bash
+sudo usermod -a -G dialout dsmr
+
+# Nginx
+sudo mkdir -p /var/www/dsmrreader/static
+sudo chown -R dsmr:dsmr /var/www/dsmrreader/
+
+# Code checkout
+git clone https://github.com/dennissiemensma/dsmr-reader.git /home/dsmr/dsmr-reader
+chown -R dsmr:dsmr /home/dsmr/
+
+# Virtual env
+sudo sudo -u dsmr mkdir /home/dsmr/.virtualenvs
+sudo sudo -u dsmr virtualenv /home/dsmr/.virtualenvs/dsmrreader --no-site-packages --python python3
+echo "source ~/.virtualenvs/dsmrreader/bin/activate" >> /home/dsmr/.bashrc
+echo "cd ~/dsmr-reader" >> /home/dsmr/.bashrc
+
+# Config & requirements
+sudo sudo -u dsmr cp /home/dsmr/dsmr-reader/dsmrreader/provisioning/django/postgresql.py /home/dsmr/dsmr-reader/dsmrreader/settings.py
+sudo sudo -u dsmr /home/dsmr/.virtualenvs/dsmrreader/bin/pip3 install -r /home/dsmr/dsmr-reader/dsmrreader/provisioning/requirements/base.txt -r /home/dsmr/dsmr-reader/dsmrreader/provisioning/requirements/postgresql.txt
+
+# Setup
+sudo sudo -u dsmr /home/dsmr/.virtualenvs/dsmrreader/bin/python3 /home/dsmr/dsmr-reader/manage.py migrate
+sudo sudo -u dsmr /home/dsmr/.virtualenvs/dsmrreader/bin/python3 /home/dsmr/dsmr-reader/manage.py collectstatic --noinput
+
+# Application user
+echo "PLEASE ENTER THE DESIRED PASSWORD (TWICE) FOR YOUR NEW 'admin' USER IN THE APPLICATION:"
+sudo sudo -u dsmr /home/dsmr/.virtualenvs/dsmrreader/bin/python3 /home/dsmr/dsmr-reader/manage.py createsuperuser --username admin --email root@localhost
+
+# Nginx
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp /home/dsmr/dsmr-reader/dsmrreader/provisioning/nginx/dsmr-webinterface /etc/nginx/sites-enabled/
+sudo service nginx configtest
+sudo service nginx reload
+
+# Supervisor
+sudo cp /home/dsmr/dsmr-reader/dsmrreader/provisioning/supervisor/dsmr-reader.conf /etc/supervisor/conf.d/
+sudo supervisorctl reread
+sudo supervisorctl update
+
+echo "Installation should be completed!"
