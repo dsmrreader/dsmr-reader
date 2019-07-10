54d9776e659fa30caa8aa95bda8d43f9e66a2686
Bram van Dartel <spam@rootrulez.com>
2019-07-10 15:55:36 +0200

    Update postgresql.py

--- dsmrreader/provisioning/django/postgresql.py
+++ dsmrreader/provisioning/django/postgresql.py
@@ -8,7 +8,9 @@ DSMRREADER_NAME = 'dsmrreader'
 DSMRREADER_USER = 'dsmrreader'
 DSMRREADER_PASSWORD = 'dsmrreader'
 DSMRREADER_HOST = 'localhost'
+DSMRREADER_PORT = 5432
 DSMRREADER_CONN_MAX_AGE = 60
+DSMRREADER_BACKEND_SLEEP = 1
 
 # Change me when exposing your application to the outside world using the Internet!
 DSMRREADER_SECRET_KEY = 'Ww\=)|a:tKxz"u@p<<Cp~MaZ%dNIYX-#w2h-*Od{>1`c%l/MJ+'
@@ -22,6 +24,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER', DSMRREADER_USER),
         'PASSWORD': os.environ.get('DB_PASS', DSMRREADER_PASSWORD),
         'HOST': os.environ.get('DB_HOST', DSMRREADER_HOST),
+        'PORT': os.environ.get('DB_PORT', DSMRREADER_PORT),
         'CONN_MAX_AGE': os.environ.get('CONN_MAX_AGE', DSMRREADER_CONN_MAX_AGE),
     }
 }
