ea4927e1fec4afd450748ac2351abc0075b9798a
Dennis Siemensma <github@dennissiemensma.nl>
2019-10-01 20:04:31 +0200

    MQTT - delete messages #724

--- dsmr_mqtt/services/messages.py
+++ dsmr_mqtt/services/messages.py
@@ -1,6 +1,7 @@
 import hashlib
 import logging
 
+from django.conf import settings
 from django.core.cache import caches
 
 from dsmr_mqtt.models import queue
@@ -13,9 +14,14 @@ def queue_message(topic, payload):
     """
     Queues a new message, but only if it doesn't exist yet and is no cached.
 
+    - Maximum message queue size will be maintained.
     - Duplicate, unsent messages will be ignored.
     - Each message sent will be cached, as it'll be dispatched with the retain flag.
     """
+
+    if queue.Message.objects.all().count() >= settings.DSMRREADER_MQTT_MAX_MESSAGES_IN_QUEUE:
+        return logger.warning('Ignoring %s to max messages in queue reached', topic)
+
     cache_storage = caches['mqtt']
 
     cache_key = '{}__{}'.format(topic, payload).encode('utf-8')
--- dsmr_mqtt/tests/services/test_messages.py
+++ dsmr_mqtt/tests/services/test_messages.py
@@ -1,6 +1,6 @@
 from unittest import mock
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from dsmr_mqtt.models import queue
 import dsmr_mqtt.services.messages
@@ -12,23 +12,37 @@ class TestMessages(TestCase):
 
     def test_okay(self):
         self.assertFalse(queue.Message.objects.exists())
-        dsmr_mqtt.services.messages.queue_message(topic=self.PAYLOAD, payload=self.TOPIC)
+        dsmr_mqtt.services.messages.queue_message(topic=self.TOPIC, payload=self.PAYLOAD)
         self.assertTrue(queue.Message.objects.exists())
+
+    @override_settings(DSMRREADER_MQTT_MAX_MESSAGES_IN_QUEUE=1)
+    @mock.patch('django.core.cache.backends.dummy.DummyCache.get')
+    def test_max(self, cache_mock):
+        # Caching disabled for this test.
+        cache_mock.return_value = None
+
+        self.assertEqual(queue.Message.objects.all().count(), 0)
+        dsmr_mqtt.services.messages.queue_message(topic=self.TOPIC, payload=1)
+        self.assertEqual(queue.Message.objects.all().count(), 1)
+
+        # Max reached, should be ignored.
+        dsmr_mqtt.services.messages.queue_message(topic=self.TOPIC, payload=2)
+        self.assertEqual(queue.Message.objects.all().count(), 1)
 
     @mock.patch('django.core.cache.backends.dummy.DummyCache.get')
     def test_cached(self, cache_mock):
         cache_mock.return_value = True
 
         self.assertFalse(queue.Message.objects.exists())
-        dsmr_mqtt.services.messages.queue_message(topic=self.PAYLOAD, payload=self.TOPIC)
+        dsmr_mqtt.services.messages.queue_message(topic=self.TOPIC, payload=self.PAYLOAD)
         self.assertFalse(queue.Message.objects.exists())
 
     def test_duplicate(self):
         self.assertEqual(queue.Message.objects.all().count(), 0)
 
-        dsmr_mqtt.services.messages.queue_message(topic=self.PAYLOAD, payload=self.TOPIC)
+        dsmr_mqtt.services.messages.queue_message(topic=self.TOPIC, payload=self.PAYLOAD)
         self.assertEqual(queue.Message.objects.all().count(), 1)
 
         # Second create should be ignored. And caching is disabled in tests as well.
-        dsmr_mqtt.services.messages.queue_message(topic=self.PAYLOAD, payload=self.TOPIC)
+        dsmr_mqtt.services.messages.queue_message(topic=self.TOPIC, payload=self.PAYLOAD)
         self.assertEqual(queue.Message.objects.all().count(), 1)
