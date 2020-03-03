87b71a73fe6861315bb7d2ec56526db210886e95
Dennis Siemensma <github@dennissiemensma.nl>
2020-03-03 20:46:31 +0100

    Updated v3.6 changelog

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-v3.6.0 - 2020-xx-xx
+v3.6.0 - 2020-02-06
 
 - [`#911 <https://github.com/dennissiemensma/dsmr-reader/issues/911>`_] Weer inzoomen in gas/temperatuur-grafieken
 - [`#912 <https://github.com/dennissiemensma/dsmr-reader/issues/912>`_] Layout verbeteren
---
+++ dsmrreader/provisioning/downgrade/v3.6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Dump for DSMR-reader v3.6
+./manage.py migrate dsmr_api 0003_create_api_user
+./manage.py migrate dsmr_backend 0014_verbose_field_translations
+./manage.py migrate dsmr_backup 0008_verbose_field_translations
+./manage.py migrate dsmr_consumption 0015_track_power_current
+./manage.py migrate dsmr_datalogger 0020_track_power_current
+./manage.py migrate dsmr_frontend 0034_mysql_timezone_support
+./manage.py migrate dsmr_mindergas 0005_schedule_mindergas_export
+./manage.py migrate dsmr_mqtt 0013_process_sleep
+./manage.py migrate dsmr_notification 0007_support_for_telegram
+./manage.py migrate dsmr_pvoutput 0002_pvoutput_latest_sync
+./manage.py migrate dsmr_stats 0014_day_total_cost_index
+./manage.py migrate dsmr_weather 0006_schedule_weather_update
