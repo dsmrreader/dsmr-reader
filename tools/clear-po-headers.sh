e8a8853113ff659d0707ba206941b32f2cd754db
Dennis Siemensma <github@dennissiemensma.nl>
2020-07-16 21:45:31 +0200

    Allow to clear PO headers from any cwd

--- tools/clear-po-headers.sh
+++ tools/clear-po-headers.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+CURRENT_DIR=$(cd `dirname $0` && pwd)
+
 
 # Remove annoying headers that get regenerated every time. And sometimes they're empty, crashing the compiler.
-sed -i '/"PO-Revision-Date:/d' dsmrreader/locales/nl/LC_MESSAGES/django.po
-sed -i '/"POT-Creation-Date:/d' dsmrreader/locales/nl/LC_MESSAGES/django.po
+sed -i '/"PO-Revision-Date:/d' $CURRENT_DIR/../dsmrreader/locales/nl/LC_MESSAGES/django.po
+sed -i '/"POT-Creation-Date:/d' $CURRENT_DIR/../dsmrreader/locales/nl/LC_MESSAGES/django.po
 
-sed -i '/"PO-Revision-Date:/d' docs/locale/nl/LC_MESSAGES/*.po
-sed -i '/"POT-Creation-Date:/d' docs/locale/nl/LC_MESSAGES/*.po
+sed -i '/"PO-Revision-Date:/d' $CURRENT_DIR/../docs/locale/nl/LC_MESSAGES/*.po
+sed -i '/"POT-Creation-Date:/d' $CURRENT_DIR/../docs/locale/nl/LC_MESSAGES/*.po
 
-sed -i '/"PO-Revision-Date:/d' docs/locale/nl/LC_MESSAGES/*/*.po
-sed -i '/"POT-Creation-Date:/d' docs/locale/nl/LC_MESSAGES/*/*.po
+sed -i '/"PO-Revision-Date:/d' $CURRENT_DIR/../docs/locale/nl/LC_MESSAGES/*/*.po
+sed -i '/"POT-Creation-Date:/d' $CURRENT_DIR/../docs/locale/nl/LC_MESSAGES/*/*.po
