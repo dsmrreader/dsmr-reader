1766e938923446e61bb956b79feaa7aaaacee717
Dennis Siemensma <github@dennissiemensma.nl>
2020-11-02 21:49:39 +0100

    Github Actions discovery

--- tools/test-all.sh
+++ tools/test-all.sh
@@ -1,25 +1,14 @@
 #!/usr/bin/env bash
 
 
-echo ""
-echo "--- Running Pylama for code audit..."
-pylama
-
-# Abort when audit fails.
-if [ $? -ne 0 ]; then
-    echo "[!] Code audit failed [!]"
-    exit;
-fi
-
-echo "OK"
-
-
+DJANGO_SETTINGS_MODULE=dsmrreader.config.test
 DJANGO_DATABASE_HOST=127.0.0.1
 DJANGO_DATABASE_USER=dsmrreader
 DJANGO_DATABASE_PASSWORD=dsmrreader
 # Will be adjusted to 'test_*' by Django.
 DJANGO_DATABASE_NAME=dsmrreader
 
+export DJANGO_SETTINGS_MODULE
 export DJANGO_DATABASE_HOST
 export DJANGO_DATABASE_USER
 export DJANGO_DATABASE_PASSWORD
@@ -30,21 +19,21 @@ echo ""
 DJANGO_DATABASE_ENGINE=django.db.backends.sqlite3
 export DJANGO_DATABASE_ENGINE
 echo "--- Testing: $DJANGO_DATABASE_ENGINE"
-pytest --cov --cov-report=html --cov-report=term --ds=dsmrreader.config.test -n 2
+pytest --cov --cov-report=html --cov-report=term -n 2
 
 
 echo ""
 DJANGO_DATABASE_ENGINE=django.db.backends.postgresql
 export DJANGO_DATABASE_ENGINE
 echo "--- Testing: $DJANGO_DATABASE_ENGINE"
-pytest --cov --cov-report=html --cov-report=term --ds=dsmrreader.config.test -n 4
+pytest --cov --cov-report=html --cov-report=term -n 4
 
 
 echo ""
 DJANGO_DATABASE_ENGINE=django.db.backends.mysql
 export DJANGO_DATABASE_ENGINE
 echo "--- Testing: $DJANGO_DATABASE_ENGINE"
-pytest --cov --cov-report=html --cov-report=term --ds=dsmrreader.config.test
+pytest --cov --cov-report=html --cov-report=term dsmr_api/tests/v1/test_api.py
 
 
 DIR=$(cd `dirname $0` && pwd)
