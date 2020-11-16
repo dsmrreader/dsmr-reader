37928687de0a1da6fa0b2247ea3919ffb378e078
Wytse Talsma <wytsetalsma@hotmail.com>
2020-11-16 10:53:40 +0100

    management command more clean

--- dsmr_backend/management/commands/dsmr_backend.py
+++ dsmr_backend/management/commands/dsmr_backend.py
@@ -2,7 +2,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from dsmr_backend.mixins import InfiniteManagementCommandMixin
+from django.utils.translation import gettext as _
 
 from dsmr_backend.backend_runner import run_mule
 
@@ -10,8 +10,17 @@ from dsmr_backend.backend_runner import run_mule
 logger = logging.getLogger('dsmrreader')
 
 
-class Command(InfiniteManagementCommandMixin, BaseCommand):
+class Command(BaseCommand):
     help = 'Backend operations in a persistent process'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--run-once',
+            action='store_true',
+            dest='run_once',
+            default=False,
+            help=_('Forces single run, overriding Infinite Command mixin')
+        )
 
     def handle(self, *args, **kwargs):
         run_mule(**kwargs)
