f5efd530b3443b5621cac1033acacf3dd7eb5961
Dennis Siemensma <github@dennissiemensma.nl>
2021-03-13 15:25:25 +0100

    Fixed doc translation header clearer #1315

--- tools/clear-po-headers.sh
+++ tools/clear-po-headers.sh
@@ -4,11 +4,5 @@ CURRENT_DIR=$(cd `dirname $0` && pwd)
 
 
 # Remove annoying headers that get regenerated every time. And sometimes they're empty, crashing the compiler.
-sed -i '/"PO-Revision-Date:/d' $CURRENT_DIR/../dsmrreader/locales/nl/LC_MESSAGES/django.po
-sed -i '/"POT-Creation-Date:/d' $CURRENT_DIR/../dsmrreader/locales/nl/LC_MESSAGES/django.po
-
-sed -i '/"PO-Revision-Date:/d' $CURRENT_DIR/../docs/_locale/nl/LC_MESSAGES/*.po
-sed -i '/"POT-Creation-Date:/d' $CURRENT_DIR/../docs/_locale/nl/LC_MESSAGES/*.po
-
-sed -i '/"PO-Revision-Date:/d' $CURRENT_DIR/../docs/_locale/nl/LC_MESSAGES/*/*.po
-sed -i '/"POT-Creation-Date:/d' $CURRENT_DIR/../docs/_locale/nl/LC_MESSAGES/*/*.po
+find $CURRENT_DIR/../ -type f -iname '*.po' | xargs sed -i '/"PO-Revision-Date:/d'
+find $CURRENT_DIR/../ -type f -iname '*.po' | xargs sed -i '/"PO-Creation-Date:/d'
