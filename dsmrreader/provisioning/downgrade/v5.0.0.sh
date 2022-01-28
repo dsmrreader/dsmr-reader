c9bb02b49079dfb5161a8c1e575e232e2b45ccd0
Dennis Siemensma <github@dennissiemensma.nl>
2022-01-29 00:03:33 +0100

    Update v5.0 migration lock

--- dsmrreader/provisioning/downgrade/v5.0.0.sh
+++ dsmrreader/provisioning/downgrade/v5.0.0.sh
@@ -3,12 +3,12 @@
 # Dump for DSMR-reader v5.0.0
 ./manage.py migrate dsmr_api 0003_create_api_user
 ./manage.py migrate dsmr_backend 0015_backend_restart_required
-./manage.py migrate dsmr_backup 0013_dropbox_setting_refactoring
+./manage.py migrate dsmr_backup 0014_dropbox_oauth
 ./manage.py migrate dsmr_consumption 0019_energy_supplier_price_decimals
 ./manage.py migrate dsmr_datalogger 0030_override_telegram_timestamp
 ./manage.py migrate dsmr_dropbox 0001_schedule_dropbox
-./manage.py migrate dsmr_frontend 0043_default_color_update_tariff_2
-./manage.py migrate dsmr_influxdb 0004_client_settings_update
+./manage.py migrate dsmr_frontend 0044_v4_20_0_release
+./manage.py migrate dsmr_influxdb 0005_influxdb_v2_support
 ./manage.py migrate dsmr_mindergas 0005_schedule_mindergas_export
 ./manage.py migrate dsmr_mqtt 0020_drop_mqtt_qos_setting
 ./manage.py migrate dsmr_notification 0008_dummy_notification_provider
