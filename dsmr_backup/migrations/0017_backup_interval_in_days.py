f6e2a93fae438bb2343ab65159fb4298a680e228
Dennis Siemensma <github@dennissiemensma.nl>
2022-11-24 23:27:51 +0100

    CS fix

--- dsmr_backup/migrations/0017_backup_interval_in_days.py
+++ dsmr_backup/migrations/0017_backup_interval_in_days.py
@@ -7,17 +7,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dsmr_backup', '0016_backup_interval_and_filename'),
+        ("dsmr_backup", "0016_backup_interval_and_filename"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='backupsettings',
-            name='backup_interval_hours',
+            model_name="backupsettings",
+            name="backup_interval_hours",
         ),
         migrations.AddField(
-            model_name='backupsettings',
-            name='backup_interval_in_days',
-            field=models.IntegerField(default=1, help_text='The minimal interval between backups. Defaults to daily.', validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(7)], verbose_name='Backup interval in days'),
+            model_name="backupsettings",
+            name="backup_interval_in_days",
+            field=models.IntegerField(
+                default=1,
+                help_text="The minimal interval between backups. Defaults to daily.",
+                validators=[
+                    django.core.validators.MinValueValidator(1),
+                    django.core.validators.MaxValueValidator(7),
+                ],
+                verbose_name="Backup interval in days",
+            ),
         ),
     ]
