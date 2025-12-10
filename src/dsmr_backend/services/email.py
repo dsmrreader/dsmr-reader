37e678fefa6aa5100b83cfd446199fa7d00790cb
Dennis Siemensma <github@dennissiemensma.nl>
2025-12-10 01:06:36 +0100

    CS

--- src/dsmr_backend/services/email.py
+++ src/dsmr_backend/services/email.py
@@ -10,7 +10,11 @@ logger = logging.getLogger("dsmrreader")
 
 
 def send(
-    email_from: str, email_to: str, subject: str, body: str, attachment: str | None = None
+    email_from: str,
+    email_to: str,
+    subject: str,
+    body: str,
+    attachment: str | None = None,
 ) -> None:
     """Sends an email using the outgoing email settings."""
     email_settings = EmailSettings.get_solo()
