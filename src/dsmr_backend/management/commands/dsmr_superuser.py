e527c61a8f5ff44ac97a1f265a65775a7ca96951
Dennis Siemensma <github@dennissiemensma.nl>
2026-01-29 20:06:16 +0100

    Fixed dsmr_superuser

--- src/dsmr_backend/management/commands/dsmr_superuser.py
+++ src/dsmr_backend/management/commands/dsmr_superuser.py
@@ -8,12 +8,8 @@ class Command(BaseCommand):  # noqa
 
     def handle(self, **options):
         """WARNING: Only safe for command line execution. Do NOT use for web requests!"""
-        username = config(
-            default=config("DSMRREADER_ADMIN_USER", default=None),
-        )
-        password = config(
-            default=config("DSMRREADER_ADMIN_PASSWORD"),
-        )
+        username = config("DSMRREADER_ADMIN_USER", default=None)
+        password = config("DSMRREADER_ADMIN_PASSWORD")
 
         if not username or not password:
             raise CommandError(
