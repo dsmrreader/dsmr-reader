c786cdaa8defc628aed77919a8b616c304c2b3a4
Dennis Siemensma <github@dennissiemensma.nl>
2026-02-12 18:55:30 +0100

    Bump v6.0 to stable

--- src/dsmrreader/__init__.py
+++ src/dsmrreader/__init__.py
@@ -4,6 +4,6 @@ from django.utils.version import get_version
 # "beta"  - Beta release - Unstable and for internal testing
 # "rc"    - Release candidate - Stable and for external pre-release testing
 # "final" - Final release - Stable and tested for everyone
-VERSION = (6, 0, 0, "rc", 12)
+VERSION = (6, 0, 0, "final", 0)
 
 __version__ = get_version(VERSION)
