ab824d9dce97da7f413e9e4712bf3c54554bb865
Dennis Siemensma <github@dennissiemensma.nl>
2026-05-15 07:21:33 +0000

    bump to 6.1.0rc3

--- src/dsmrreader/__init__.py
+++ src/dsmrreader/__init__.py
@@ -4,6 +4,6 @@ from django.utils.version import get_version
 # "beta"  - Beta release - Unstable and for internal testing
 # "rc"    - Release candidate - Stable and for external pre-release testing
 # "final" - Final release - Stable and tested for everyone
-VERSION = (6, 1, 0, "rc", 2)
+VERSION = (6, 1, 0, "rc", 3)
 
 __version__ = get_version(VERSION)
