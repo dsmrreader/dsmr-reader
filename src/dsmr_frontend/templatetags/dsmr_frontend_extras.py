78c71da948909599bec88e82516245adddd02984
Dennis Siemensma <github@dennissiemensma.nl>
2026-06-02 21:28:49 +0000

    style: black reformat

--- src/dsmr_frontend/templatetags/dsmr_frontend_extras.py
+++ src/dsmr_frontend/templatetags/dsmr_frontend_extras.py
@@ -36,4 +36,6 @@ def decimal_html(value: object) -> SafeData:
     sep = text[sep_pos]
     integer_part = text[:sep_pos]
     decimal_part = text[sep_pos + 1 :]
-    return format_html('<span class="badge-integer">{}</span><span class="badge-decimal">{}{}</span>', integer_part, sep, decimal_part)
+    return format_html(
+        '<span class="badge-integer">{}</span><span class="badge-decimal">{}{}</span>', integer_part, sep, decimal_part
+    )
